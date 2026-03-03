8559b4fb7af232b6e48ccc45dc0a82d7c8111c3f
Ryma Djoudad <zenyod85@gmail.com>
2026-03-03 10:23:55 -0500

    Added shirt with custom size and message

--- tshirt.py
+++ tshirt.py
@@ -7,3 +7,15 @@ on the shirt. The function should print a sentence summarizing the size of the s
 Call the function once using positional arguments to make a shirt. Call the function a second time
 using keyword arguments.
 """
+def make_shirt(size, message):
+    """Make a shirt with a size and a message"""
+    print(f"A shirt of the size {size.upper()} will have the following message printed on it: \n{message}\n")
+
+make_shirt("XL", "Hang in there")
+
+"""
+8-4
+Large Shirts
+Modify the make_shirt() function so that shirts are large by default with a message that reads "I love Python". 
+Make a large shirt and a medium shirt with the default message, and a shirt of any size with a different message.
+"""
