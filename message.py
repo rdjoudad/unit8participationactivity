4c05a61adacab015dfc9750f56245a2ee677793c
Ryma Djoudad <zenyod85@gmail.com>
2026-03-08 18:40:24 -0400

    8-10

--- message.py
+++ message.py
@@ -6,8 +6,37 @@ text message
 """
 
 def show_messages(short_messages):
+    if not short_messages:
+        print("no messages to print")
+        return
     for message in short_messages:
         print(message)
 
-short_messages = ["Hello", "How's it going?", "How's work?"]
+def send_messages(unsent_messages, sent_messages):
+    while unsent_messages:
+        message = unsent_messages.pop()
+        print(f"Sending:\n{message}")
+        print()
+        sent_messages.append(message)
+
+short_messages = ["Hello", "How's it going?", "Lovely weather we're having"]
+sent_messages = []
+send_messages(short_messages, sent_messages)
+
 show_messages(short_messages)
+show_messages(sent_messages)
+
+"""
+8-10. 
+Sending Messages
+Start with a copy of your program from Exercise 8-9. Write a function called send_messages() that prints each text message and moves each message to a new list
+called sent_messages as it's printed. After calling the function, print both of your lists to make sure the messages
+were moved correctly.
+"""
+
+def send_messages(unsent_messages, sent_messages):
+    while unsent_messages:
+        message = unsent_messages.pop
+        print(f"Sending:\n{message}")
+        print()
+        send_messages.append(message)
