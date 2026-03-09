7fb260611b776777e952db6a3f708b1101cc46bc
Ryma Djoudad <zenyod85@gmail.com>
2026-03-09 16:54:39 -0400

    Added one more message

--- message.py
+++ message.py
@@ -42,7 +42,7 @@ Archived Messages
 Start with your work from Exercise 8-10. Call the function send_messages() with a copy of the list of messages. 
 After calling the function, print both of your lists to show that the original list has retained its messages.
 """
-short_messages = ["Hello", "How's it going?", "Lovely weather we're having"]
+short_messages = ["Hello", "How's it going?", "Lovely weather we're having", "How's work?"]
 sent_messages = []
 
 send_messages(short_messages[:], sent_messages)
